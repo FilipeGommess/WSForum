7482ce6f85e896f03561997509b219a9dcd32553
filipegommess <filipepachecogomes2006@gmail.com>
2025-03-01 12:53:47 -0300

    fix: host ip

--- api/server.js
+++ api/server.js
@@ -3,10 +3,11 @@ import { MessageController } from './controllers/MessageController.js';
 import mongoose from 'mongoose';
 import 'dotenv/config'
 
-const wss = new WebSocketServer({ port: 3000 });
+const wss = new WebSocketServer({ port: 3000, host: '0.0.0.0' });
 
 try {
   await mongoose.connect(process.env.DATABASE_URL);
+  console.log('Connected')
 } catch (e) {
   console.error(e);
 }
